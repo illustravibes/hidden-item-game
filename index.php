af10e54106b9a865afeab1e9e44361c312f38905
Fariz <farizsansms@gmail.com>
2026-07-08 10:36:04 +0700

    add findPlayer method

--- index.php
+++ index.php
@@ -17,7 +17,26 @@ class HiddenItemGame {
             echo implode('', $row) . PHP_EOL;
         }
     }
+
+    public function findPlayer(): ?array {
+    foreach ($this->grid as $rowIndex => $row) {
+        foreach ($row as $colIndex => $cell) {
+            if ($cell === 'X') {
+                return [
+                    'row' => $rowIndex,
+                    'col' => $colIndex,
+                ];
+            }
+        }
+    }
+
+    return [];
+}
 }
 
 $game = new HiddenItemGame();
 $game->render();
+
+$player = $game->findPlayer();
+
+print_r($player);
